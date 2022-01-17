e1af820528fb939e29742bb961e37393d7ab49e2
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-17 21:30:14 +0200

    Web-сервер з HTML-сторінками

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
 
-    res.end('<h1>Hello World</h1>');
+    if (req.url === "/") {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(data);
+        })
+    } else if (req.url === "/contact") {
+        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(data);
+        })
+    }
 })
 
 server.listen(3001, () => {

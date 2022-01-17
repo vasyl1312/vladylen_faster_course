0c62258422f63a25251e0635b220c148c446de0c
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-17 22:17:00 +0200

    Створення  простого REST-сервера

--- index.js
+++ index.js
@@ -4,23 +4,49 @@ const path = require('path');
 
 const server = http.createServer((req, res) => {
 
-    if (req.url === "/") {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.end(data);
-        })
-    } else if (req.url === "/contact") {
-        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.end(data);
-        })
+    // if (req.url === "/") {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data);
+    //     })
+    // } else if (req.url === "/contact") {
+    //     fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data);
+    //     })
+    // }
+    let filePath = path.join(__dirname, 'public', req.url === "/" ? 'index.html' : req.url);
+    const ext = path.extname(filePath);
+    if (!ext) {
+        filePath += '.html';
     }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500);
+                    res.end('Error');
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.end(data);
+                }
+            });
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(content);
+        }
+    })
 })
 
 server.listen(3001, () => {

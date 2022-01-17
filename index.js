0386e7e3487ba375368d4a5b4ba5446294d5dd74
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-17 22:36:43 +0200

    Работа з консоллю v і зміна бекграунду

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ const server = http.createServer((req, res) => {
     // }
     let filePath = path.join(__dirname, 'public', req.url === "/" ? 'index.html' : req.url);
     const ext = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
     if (!ext) {
         filePath += '.html';
     }
@@ -42,7 +54,7 @@ const server = http.createServer((req, res) => {
             });
         } else {
             res.writeHead(200, {
-                'Content-Type': 'text/html'
+                'Content-Type': contentType
             });
             res.end(content);
         }
